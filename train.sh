7b445a0a3a66f502c4bb1229611ea85bee18a02a
123 <123@123.com>
2024-11-13 13:49:10 +0800

    update scripts

---
+++ train.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+export TORCH_CPP_LOG_LEVEL=INFO NCCL_DEBUG=INFO
+export CUDA_VISIBLE_DEVICES="0,1,2,3,4,5"
+
+task="sft" 
+
+ACCELERATE_LOG_LEVEL=info accelerate launch \
+--config_file training_configs/deepspeed_zero3.yaml \
+${task}_only_out.py \
+training_configs/${task}_full_only_out.yaml
+
+
+task="dpo" 
+
+ACCELERATE_LOG_LEVEL=info accelerate launch \
+--config_file training_configs/deepspeed_zero3.yaml \
+${task}.py \
+training_configs/${task}_full_align_llama2.yaml
